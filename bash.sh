02fb21dac85d60472e1a2c2d15515995daa8f6a1
root <root@ip-172-31-6-201.ap-south-1.compute.internal>
2025-07-10 08:25:09 +0000

    newupdate

--- bash.sh
+++ bash.sh
@@ -1,6 +1,7 @@
 yum install -y yum-utils shadow-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 yum -y install terraform
+aws configure
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 amazon-linux-extras install java-openjdk11 -y
